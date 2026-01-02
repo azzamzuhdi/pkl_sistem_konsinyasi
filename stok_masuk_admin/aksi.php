1905287da719ae0a276a9d5021b652e875dc5c28
azzamzps <azzamtik25@gmail.com>
2026-01-02 19:32:00 +0700

    bug fixes(3)

--- stok_masuk_admin/aksi.php
+++ stok_masuk_admin/aksi.php
@@ -8,7 +8,6 @@ if (isset($_POST['tambah_stok_masuk'])) {
     $tanggal = date("Y-m-d H:i:s");
 
     mysqli_query($conn, "INSERT INTO tb_stok_masuk (id_supplier, id_barang, jumlah_masuk, tanggal_masuk) VALUES ('$id_supplier', '$id_barang', '$jumlah_stok', '$tanggal')") or die(mysqli_error($conn));
-    mysqli_query($conn, "UPDATE tb_barang SET stok_masuk = stok_masuk + '$jumlah_stok' WHERE id_barang = '$id_barang' ") or die(mysqli_error($conn));
     mysqli_query($conn, "UPDATE tb_barang SET stok_masuk = stok_masuk + '$jumlah_stok', sisa_stok = sisa_stok + '$jumlah_stok' WHERE id_barang = '$id_barang' ") or die(mysqli_error($conn));
 
     echo "<script>alert('Stok berhasil ditambahkan');window.location='index.php?id_supplier=$id_supplier';</script>";
